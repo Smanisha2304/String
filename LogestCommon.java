833c79d490d7811c7eb1a02f326e8966a8a35c19
Manisha Sharma <alwsmilemansu23@gmail.com>
2025-08-13 23:38:04 +0530

    Add files via upload

---
+++ LogestCommon.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+class Solution {
+    public String longestCommonPrefix(String[] strs)
+    {
+         Arrays.sort(strs);
+         String s1 = strs[0];
+         String s2 = strs[strs.length-1];
+         int count = 0;
+         while(count<s1.length() && count<s2.length())
+         {
+            if(s1.charAt(count)==s2.charAt(count))
+            {
+                count++;
+            }
+            else{
+                break;
+            }
+
+         }
+         String ss = s1.substring(0,count);
+         return ss;
+    }
+}
