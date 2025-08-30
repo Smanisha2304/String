2bf33e008aa66000b37b128ffb2638985d090113
Manisha Sharma <alwsmilemansu23@gmail.com>
2025-08-30 23:49:07 +0530

    Length.java

--- Length.java
+++ Length.java
@@ -2,9 +2,10 @@ class Solution {
     public int lengthOfLastWord(String s) {
         s= s.trim();
         String[] parts = s.split(" ");
-        // System.out.println(parts[0]);
+         // System.out.println(parts[0]);
         String lastword = parts[parts.length-1];
         int count = lastword.length();
         return count;    
     }
+
 }
